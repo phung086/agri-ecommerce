eaf37a41ee73a82b37b0fa36b2632ba618fb54a0
phung086 <22010145@st.phenikaa-uni.edu.vn>
2026-06-17 15:05:32 +0700

    feat: add jwt security configuration

---
+++ agri-ecommerce-backend/src/main/java/com/agri/ecommerce/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.agri.ecommerce.security;
+
+import com.agri.ecommerce.entity.UserEntity;
+import com.agri.ecommerce.exception.ResourceNotFoundException;
+import com.agri.ecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy người dùng với email: " + email));
+
+        return UserPrincipal.create(user);
+    }
+
+    public UserDetails loadUserById(Long id) {
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy người dùng với id: " + id));
+
+        return UserPrincipal.create(user);
+    }
+}
